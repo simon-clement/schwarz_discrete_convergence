9044d390eba0d393af6508f6d618db95493d2b95
Simon Clément <simon.clement@ensimag.fr>
2019-03-18 16:44:16 +0100

    added integration in time and examples

--- cv_rate.py
+++ cv_rate.py
@@ -1,21 +1,122 @@
 import numpy as np
 
+H_DEFAULT = 1e-4
+D_DEFAULT = 1.0
+A_DEFAULT = 0.0
+C_DEFAULT = 0.0
 
 """
-    Computation of u*
+    Solve the equation with full system:
+    \partial t u* + Y* u* = f*
+    and return u* at each time step.
+
+    f_star_0 is the value of f* in x0.
+    The other values of f_star are deduced from f1 and f2
+
+    This function should be useless in the future but it is a good example
+    of how to use the integrator in time.
 """
-def solver_original_eq(Y):
-    pass
+def solve_u_time_domain(u1_init, u2_init,
+                        f_star_0, f1, f2,
+                      Lambda_1, Lambda_2,
+                      D1, D2,
+                      h1, h2,
+                      a, c, dt, number_time_steps):
+    assert type(float(f_star_0)) == float
+    assert type(float(dt)) == float
+    assert u1_init.ndim == u2_init.ndim == f1.ndim == f2.ndim == 1
+    M1 = u1_init.shape[0]
+    M2 = u2_init.shape[0]
+    assert f1.shape[0] == M1 and f2.shape[0] == M2
+    assert type(float(a)) == float
+    assert type(float(c)) == float
+    assert type(float(Lambda_1)) == float
+    assert type(float(Lambda_2)) == float
+    # Broadcasting of h and D:
+    h1 = np.ones(M1-1) * h1
+    h2 = np.ones(M2-1) * h2
+    D1 = np.ones(M1-1) * D1
+    D2 = np.ones(M2-1) * D2
+    assert (h1 < 0).all()
+    assert (h2 > 0).all()
+    assert (D1 > 0).all()
+    assert (D2 > 0).all()
+    assert h1.ndim == D1.ndim == h2.ndim == D2.ndim == 1
+
+    # Compute Y matrices:
+    M_star = M1 + M2 - 1
+    h_star = np.concatenate((-h1[::-1], h2))
+    D_star = np.concatenate((D1[::-1], D2))
+    Y_star = get_Y_star(M_star=M_star, h_star=h_star, D_star=D_star, a=a, c=c)
+
+    f_star = np.concatenate(([f1[-1]],-f1[-2:0:-1], [f_star_0], f2[1:]))
+    #we use -f1 because f1 is f*(h^1_m + h^1_{m-1}) and h^1<0
+    u0_star = np.concatenate((u1_init[:0:-1], u2_init))
+    f0_on_time = np.array([f1[-1] for _ in range(number_time_steps)])
+
+    return integration(u0_star, Y_star, f_star, f0_on_time, dt, 1)
+
+"""
+    Performs integration in time with a theta-shema.
+    u0 is the initial condition
+    Y is the matrix returned by get_Y or get_Y_star, such that
+    \partial t u + Yu = f
+    f cannot evolve in time except its first coordinate f_x0:
+    f must have the shape of u
+    f_x0 must be a 1-D array
+    the number of time_steps is (f_x0.shape[0] - 1)
+"""
+def integration(u0, Y, f_const, f_x0, dt, theta):
+    f = np.copy(f_const)
+    assert Y[0].ndim == Y[1].ndim == Y[2].ndim == 1
+    assert f_const.ndim == f.ndim == u0.ndim == 1
+    M = Y[1].shape[0]
+    assert f_const.shape[0] == u0.shape[0] == M
+    assert Y[0].shape[0] == Y[2].shape[0] == M - 1
+    assert 0 <= theta <= 1
+
+    u = np.empty((f_x0.shape[0], u0.shape[0]))
+    u[0] = u0
+    if theta != 0:
+        for step in range(f_x0.shape[0] - 1):
+            f[0] = (1-theta) * f_x0[step] + theta * f_x0[step+1]
+            Y_implicit = (theta*Y[0], theta*Y[1] + np.ones(M)/dt, theta*Y[2])
+            Yu = multiply(Y, u[step])
+            u[step+1] = solve_linear(Y_implicit,
+                                          f + u[step] / dt - (1-theta)*Yu)
+    else: #explicit case:
+        for step in range(f_x0.shape[0] - 1):
+            f[0] = f_x0[step]
+            Yu = multiply(Y, u[step])
+            u[step+1] = u[step] + dt*(f - Yu)
+    return u
+
+"""
+    Returns "Y * u"
+    equivalent code :
+    return (np.diag(Y[1])+np.diag(Y[0], k=-1) + np.diag(Y[2], k=1)) @ u
+    Y is a tridiagonal matrix returned by get_Y or get_Y_star
+"""
+def multiply(Y, u):
+    assert len(Y) == 3
+    assert u.ndim == Y[0].ndim == Y[1].ndim == Y[2].ndim == 1
+    assert Y[1].shape[0] == Y[0].shape[0] + 1 == u.shape[0]
+    assert Y[0].shape[0] == Y[2].shape[0]
+    return np.concatenate(([0], Y[0]*u[:-1])) + Y[1] * u + \
+            np.concatenate((Y[2]*u[1:], [0]))
 
 
 """
     Solve the linear system Yu = f and returns u.
-    This function is just a wrapper to scipy
+    This function is just a wrapper over scipy
     Y is a tuple (Y_0, Y_1, Y_2) containing respectively:
     - The left diagonal of size M-1
     - The main diagonal of size M
     - The right diagonal of size M-1
-    f is an array of size M
+    f is an array of size M.
+    f[0] is the condition on the bottom of the domain
+    f[-1] is the condition on top of the domain
+    /!\ f[1:-1] should be equal to f * (hm + hmm1) /!\
 
     Y is returned by the functions get_Y and get_Y_star
 """
@@ -26,17 +127,15 @@ def solve_linear(Y, f):
     assert Y_1.shape[0] - 1 == Y_2.shape[0] == Y_0.shape[0] == f.shape[0] - 1
 
     # solve_banded function requires to put the diagonals in the following form:
-    Y = np.vstack(([0] + Y_2, Y_1, Y_0 + [0]))
+    Y_2 = np.concatenate(([0], Y_2))
+    Y_0 = np.concatenate((Y_0, [0]))
+    Y = np.vstack((Y_2, Y_1, Y_0))
     from scipy.linalg import solve_banded
-    return solve_banded((1, 1), Y, f, overwrite_ab=True, overwrite_b=False)
+    return solve_banded((1, 1), Y, f)
 
-H_DEFAULT = 1e-4
-D_DEFAULT = 1.0
-A_DEFAULT = 0.0
-C_DEFAULT = 0.0
 """
     Returns the tridiagonal matrix Y in the shape asked by solve_linear.
-    (Does not actually returns a np matrix, it returns (Y_0, Y_1, Y_2). 
+    (Does not actually returns a np matrix, it returns (Y_0, Y_1, Y_2).
     For details, see the documentation of @solve_linear)
 
     The returned matrix is of dimension MxM
@@ -44,7 +143,7 @@ C_DEFAULT = 0.0
 
     The following parameters can either be given as floats or np.ndarray
 
-    h: step size (always positive) (size: M-1)
+    h: step size (>0 in Omega_2, <0 in Omega_1) (size: M-1)
     D: diffusivity (always positive) (size: M-1)
         Note: if D is a np.ndarray, it should be given on the half-steps,
                 i.e. D[m] is D_{m+1/2}
@@ -52,7 +151,7 @@ C_DEFAULT = 0.0
     c: reaction coefficient (should be positive) (size: M-2)
 
     If upper_domain is True, returns Y2; otherwise returns Y1
-    (The upper_domain is \Omega_2)
+    (The upper domain is \Omega_2)
 
     /!\ WARNING : DUPLICATE CODE between get_Y_star and get_Y (for lisibility)
 
@@ -75,12 +174,21 @@ def get_Y(M, Lambda, h=H_DEFAULT, D=D_DEFAULT, a=A_DEFAULT,
                 type(arg) is np.ndarray and \
                 arg.ndim == 1 and arg.shape[0] == M-1
     assert type(upper_domain) is bool
+
     # Broadcast or verification of size:
     D = np.zeros(M-1) + D
     h = np.zeros(M-1) + h
-    # In the notations h is negative when considering \omega_1:
-    if not upper_domain :
-        h = -h
+
+    if (np.array(h) <= 0).any() and upper_domain:
+        print("Warning : h should not be negative in Omega_2")
+    if (np.array(h) >= 0).any() and not upper_domain:
+        print("Warning : h should not be positive in Omega_1")
+    if (np.array(D) < 0).any():
+        print("Warning : D should never be negative")
+    if a < 0:
+        print("Warning : a should probably not be negative")
+    if c < 0:
+        print("Warning : c should probably not be negative")
 
     h_m = h[1:] # h_{m}
     h_mm1 = h[:-1] # h_{m-1}
@@ -90,26 +198,27 @@ def get_Y(M, Lambda, h=H_DEFAULT, D=D_DEFAULT, a=A_DEFAULT,
     ######## MAIN DIAGONAL
     Y_1 = np.empty(M)
 
-    Y_1[1:M-1] = (c - a / h_m)*sum_both_h + 2*(h_mm1*D_mp1_2 + h_m* D_mm1_2) / \
+    Y_1[1:M-1] = c*sum_both_h + 2*(h_mm1*D_mp1_2 + h_m* D_mm1_2) / \
             (h_m*h_mm1)
     Y_1[0] = Lambda - D[0] / h[0] # Robin bd conditions at interface
     Y_1[M-1] = 1 # Neumann bd conditions for \Omega_2, Dirichlet for \Omega_1
+    if upper_domain:
+        Y_1[M-1] /= h[-1]
 
     ######## RIGHT DIAGONAL
     Y_2 = np.empty(M-1)
     Y_2[1:] = -2 * D_mp1_2 / h_m
     Y_2[0] = D[0] / h[0]
-    if upper_domain:
-        Y_2[1:] += sum_both_h * a / h_m
+    Y_2[1:] += a
 
     ######## LEFT DIAGONAL
     Y_0 = np.empty(M-1)
     Y_0[:-1] = -2 * D_mm1_2 / h_mm1
+    Y_0[:-1] -= a
     if not upper_domain:
-        Y_0[:-1] += sum_both_h * a / h_m # in \Omega_1 we add a/h_{m}...
-        Y_0[-1] = 0 # and we have Dirichlet bd conditions
-    else: # In \Omega_2 we have Neumann bd conditions
-        Y_0[-1] = -1
+        Y_0[-1] = 0 # In \Omega_1 we have Dirichlet bd conditions
+    else:
+        Y_0[-1] = -1/h[-1] # In \Omega_2 we have Neumann bd conditions
 
     return (Y_0, Y_1, Y_2)
 
@@ -145,7 +254,7 @@ def get_Y(M, Lambda, h=H_DEFAULT, D=D_DEFAULT, a=A_DEFAULT,
     /!\ WARNING : DUPLICATE CODE between get_Y_star and get_Y (for lisibility)
 
 """
-def get_Y_star(M_star, h_star=H_DEFAULT, D_star=D_DEFAULT, 
+def get_Y_star(M_star, h_star=H_DEFAULT, D_star=D_DEFAULT,
         a=A_DEFAULT, c=C_DEFAULT):
     M = M_star
     h = h_star
@@ -164,6 +273,15 @@ def get_Y_star(M_star, h_star=H_DEFAULT, D_star=D_DEFAULT,
         assert type(arg) is float or \
                 type(arg) is np.ndarray and \
                 arg.ndim == 1 and arg.shape[0] == M-1
+    if (np.array(h) < 0).any():
+        print("Warning : h should never be negative")
+    if (np.array(D) < 0).any():
+        print("Warning : D should never be negative")
+    if a < 0:
+        print("Warning : a should probably not be negative")
+    if c < 0:
+        print("Warning : c should probably not be negative")
+
     # Broadcast or verification of size:
     D = np.zeros(M-1) + D
     h = np.zeros(M-1) + h
@@ -177,25 +295,26 @@ def get_Y_star(M_star, h_star=H_DEFAULT, D_star=D_DEFAULT,
     ######## MAIN DIAGONAL
     Y_1 = np.empty(M)
 
-    Y_1[1:M-1] = (c - a / h_m)*sum_both_h + 2*(h_mm1*D_mp1_2 + h_m* D_mm1_2) / \
+    Y_1[1:M-1] = c*sum_both_h + 2*(h_mm1*D_mp1_2 + h_m* D_mm1_2) / \
             (h_m*h_mm1)
     Y_1[0] = 1 # Dirichlet
-    Y_1[M-1] = 1 # Neumann
+    Y_1[M-1] = 1/h[-1] # Neumann
 
     ######## RIGHT DIAGONAL
     Y_2 = np.empty(M-1)
-    Y_2[1:] = -2 * D_mp1_2 / h_m + sum_both_h * a / h_m
+    Y_2[1:] = -2 * D_mp1_2 / h_m + a
     Y_2[0] = 0
 
     ######## LEFT DIAGONAL
     Y_0 = np.empty(M-1)
-    Y_0[:-1] = -2 * D_mm1_2 / h_mm1
-    Y_0[-1] = -1 # Neumann bd conditions on top
+    Y_0[:-1] = -2 * D_mm1_2 / h_mm1 - a
+    Y_0[-1] = -1/h[-1] # Neumann bd conditions on top
 
     return (Y_0, Y_1, Y_2)
 
 if __name__ == "__main__":
-    from tests.test_linear_sys import test_get_Y
-    print("Test get_Y:", test_get_Y())
-    # TODO automatic test on constant coeff. heat eq
-    # TODO manual test on non constant coeff. heat eq
+    import tests.test_linear_sys
+    import tests.test_schwarz
+    tests.test_linear_sys.launch_all_tests()
+    tests.test_schwarz.launch_all_tests()
+
--- tests/test_linear_sys.py
+++ tests/test_linear_sys.py
@@ -1,6 +1,119 @@
 import numpy as np
+from numpy import cos, sin
 from numpy.random import random
-from cv_rate import get_Y, get_Y_star
+from cv_rate import get_Y, get_Y_star, solve_linear
+
+"""
+    Test of the solver in with D!= 0, a != 0 and c!=0.
+    The equation is solved analytically and compared
+    to the numerical approximation.
+    we choose u* = sin(dx) on [0,1]
+    h is not constant
+    f will be d^2 D(x)*sin(dx) - dD'(x)*cos(dx) + da*cos(dx) + c*sin(dx)
+"""
+def test_full_eq():
+    # Our domain is [0,1]
+    M = 10001
+    a = 1.2
+    c = 0.3
+    d = 1.2
+
+    x = np.linspace(0,1,M)**3
+    h = np.diff(x)
+    hm1 = h[:-1]
+    hm = h[1:]
+    middle_grid_last = (x[-1] + x[-2])/2
+
+    D = lambda x: x
+    D_prime = lambda x: 1
+    D_star = D((x[1:] + x[:-1])/2)
+
+
+    function_f = lambda x : d*d*D(x)*sin(d*x) - d*D_prime(x)*cos(d*x) \
+        + d*a*cos(d*x) + c*sin(d*x)
+    f = np.zeros(M)
+    f[1:M-1] = function_f(x[1:M-1]) * (hm1 + hm)
+    f[-1] = d*cos(d*middle_grid_last)
+
+    Y = get_Y_star(M_star=M, h_star=h, D_star=D_star, a=a, c=c)
+    assert np.linalg.norm(sin(d*x) - solve_linear(Y, f)) < 1e-7
+
+    Y = get_Y(M=M, Lambda=1.0, h=h, D=D_star, a=a, c=c, upper_domain=True)
+    f = np.zeros(M)
+    f[0] = d*cos(d*x[1]/2)*D_star[0] # Robin bd condition, u(0) = 0
+    f[1:M-1] = function_f(x[1:M-1]) * (hm1 + hm)
+    f[-1] = d*cos(d*middle_grid_last) # Neumann bd condition
+    assert np.linalg.norm(sin(d*x) - solve_linear(Y, f)) < 1e-7
+
+    # Our domain is now [-1,0]
+    x = np.linspace(0,-1,M)**3
+    h = np.diff(x)
+    hm1 = h[:-1]
+    hm = h[1:]
+
+    D = lambda x: -x
+    D_prime = lambda x: -1
+    D_star = D((x[1:] + x[:-1])/2)
+    function_f = lambda x : d*d*D(x)*sin(d*x) - d*D_prime(x)*cos(d*x) \
+        + d*a*cos(d*x) + c*sin(d*x)
+
+    Y = get_Y(M=M, Lambda=1.0, h=h, D=D_star, a=a, c=c, upper_domain=False)
+    f = np.zeros(M)
+    f[0] = D_star[0] * d*cos(d*(x[1]/2)) # Robin bd condition, u(0) = 0
+    f[1:M-1] = function_f(x[1:M-1]) * (hm1 + hm)
+    f[-1] = sin(d*x[-1]) # Dirichlet bd condition
+    assert np.linalg.norm(sin(d*x) - solve_linear(Y, f)) < 2e-6
+    return "ok"
+
+"""
+    Test of the solver in with a=c=0.
+    The equation is solved analytically and compared
+    to the numerical approximation.
+    tested for D constant and u* = x, u* = x^2/2
+    for D(x) = x and u* = x.
+    h is not constant.
+"""
+def test_heat_eq():
+    # Our domain is [0,1]
+    M = 401
+    x = np.linspace(0,1,M)**6
+    h = np.diff(x)
+    hm1 = h[:-1]
+    hm = h[1:]
+    middle_grid_last = (x[-1] + x[-2])/2
+    D = 1.0
+    a = 0.0
+    c = 0.0
+    Y = get_Y_star(M_star=M, h_star=h, D_star=D, a=a, c=c)
+
+    # Simplest case: f=0, u* = x
+    f = np.zeros(M)
+    f[1:M-1] = 0 * (hm1 + hm)
+    f[-1] = np.pi
+    assert np.linalg.norm(np.pi*x - solve_linear(Y, f)) < 1e-10
+
+    # almost simplest case: f=-1, u* = x^2/2
+    f[0] = 0 # x[1:M-1]
+    f[1:M-1] = -1 * (hm1 + hm)
+    f[-1] = middle_grid_last
+    # real_Y = np.diag(Y[2], k=1) + np.diag(Y[1]) + np.diag(Y[0], k=-1)
+    u = solve_linear(Y, f)
+    assert np.linalg.norm(x*x / 2 - solve_linear(Y, f)) < 1e-10
+
+
+    # now we take D = x (don't forget D[i] = D((x_i+x_{i+1})/2)
+    D = (x[1:] + x[:-1]) / 2
+    Y = get_Y_star(M_star=M, h_star=h, D_star=D, a=a, c=c)
+
+    # almost simplest case: f=-1, D(x)=x, u* = x
+    f[0] = 0 # x[1:M-1]
+    f[1:M-1] = -1 * (hm1 + hm)
+    f[-1] = 1
+    # real_Y = np.diag(Y[2], k=1) + np.diag(Y[1]) + np.diag(Y[0], k=-1)
+    u = solve_linear(Y, f)
+    assert np.linalg.norm(x - solve_linear(Y, f)) < 1e-10
+    return "ok"
+
 """
     Test of the function get_Y:
     check the relation Y_1 = 2hc - Y_0 - Y_1
@@ -17,9 +130,9 @@ def test_get_Y():
                 upper_domain=True)
         # constant coefficients case:
         for i in range(M-2):
-            assert abs(Y_1[i+1] - (4*D/h - 2*a + 2*h*c)) < FLOAT_TOL
-            assert abs(Y_0[i+0] - (-2*D/h)) < FLOAT_TOL
-            assert abs(Y_2[i+1] - (-2*D/h+2*a)) < FLOAT_TOL
+            assert abs(Y_1[i+1] - (4*D/h + 2*h*c)) < FLOAT_TOL
+            assert abs(Y_0[i+0] - (-2*D/h-a)) < FLOAT_TOL
+            assert abs(Y_2[i+1] - (-2*D/h+a)) < FLOAT_TOL
 
         assert abs(Y_2[0] + Y_1[0] - Lambda) < FLOAT_TOL # Robin
         assert abs(Y_0[-1] + Y_1[-1]) < FLOAT_TOL # Neumann
@@ -31,14 +144,14 @@ def test_get_Y():
 
     for _ in range(1000): # tests of multiple coeffs: \Omega_1
         a, c, h, D, Lambda = [abs(random())+1e-12 for _ in range(5)]
+        h = -h
         Y_0, Y_1, Y_2 = get_Y(M=M, Lambda=Lambda, h=h, D=D, a=a, c=c,
                 upper_domain=False)
-        h = -h
         # constant coefficients case:
         for i in range(M-2):
-            assert abs(Y_1[i+1] - (4*D/h - 2*a + 2*h*c)) < FLOAT_TOL
-            assert abs(Y_0[i+0] - (-2*D/h+2*a)) < FLOAT_TOL
-            assert abs(Y_2[i+1] - (-2*D/h)) < FLOAT_TOL
+            assert abs(Y_1[i+1] - (4*D/h + 2*h*c)) < FLOAT_TOL
+            assert abs(Y_0[i+0] - (-2*D/h-a)) < FLOAT_TOL
+            assert abs(Y_2[i+1] - (-2*D/h+a)) < FLOAT_TOL
 
         assert abs(Y_2[0] + Y_1[0] - Lambda) < FLOAT_TOL # Robin
         assert abs(Y_0[-1]) < FLOAT_TOL # Dirichlet
@@ -53,9 +166,9 @@ def test_get_Y():
         Y_0, Y_1, Y_2 = get_Y_star(M_star=M, h_star=h, D_star=D, a=a, c=c)
         # constant coefficients case:
         for i in range(M-2):
-            assert abs(Y_1[i+1] - (4*D/h - 2*a + 2*h*c)) < FLOAT_TOL
-            assert abs(Y_0[i+0] - (-2*D/h)) < FLOAT_TOL
-            assert abs(Y_2[i+1] - (-2*D/h+2*a)) < FLOAT_TOL
+            assert abs(Y_1[i+1] - (4*D/h + 2*h*c)) < FLOAT_TOL
+            assert abs(Y_0[i+0] - (-2*D/h-a)) < FLOAT_TOL
+            assert abs(Y_2[i+1] - (-2*D/h+a)) < FLOAT_TOL
 
         assert abs(Y_2[0]) < FLOAT_TOL # Dirichlet
         assert abs(Y_1[0] - 1) < FLOAT_TOL # Dirichlet
@@ -65,5 +178,10 @@ def test_get_Y():
         Y_2 = Y_2[1:]
         assert np.linalg.norm(Y_1 - 2*h*c + Y_0 + Y_2) < FLOAT_TOL*M
 
-
     return "ok"
+
+def launch_all_tests():
+    print("Test get_Y:", test_get_Y())
+    print("Test heat equation:", test_heat_eq())
+    print("Test parabolic equation:", test_full_eq())
+
