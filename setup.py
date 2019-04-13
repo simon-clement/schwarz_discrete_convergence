5ace8f9a220c3f6a215d5077f40b32e660c95909
Simon Clement <simon.clement@grenoble-inp.org>
2019-04-13 15:17:04 +0200

    added gfortran and liblapack dependency

--- setup.py
+++ setup.py
@@ -7,11 +7,13 @@ import os
 os.system('pip3 install scipy')
 try:
     os.system('curl https://sh.rustup.rs -sSf | sh')
-    os.system('sudo apt install libopenblas-base')
+    os.system('sudo apt install gfortran')
+    os.system('sudo apt install libopenblas-dev')
+    os.system('sudo apt install liblapack-dev')
     os.system('pip3 install cffi')
     os.system("echo \'# added by setup script of schwarz_tbc_finder\'" +
               ">> ~/.bashrc")
     os.system("echo \'export PATH=~/.cargo/bin:$PATH\'" + ">> ~/.bashrc")
-except:
+except BaseException:
     print("Cannot install fast version.")
     print("The code will be able to run but slowly.")
