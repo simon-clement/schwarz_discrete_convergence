d8e87f791355bb0634645fa89dabd8322197f2b4
Simon Clement <simon.clement@grenoble-inp.org>
2021-12-14 12:56:17 +0100

    figure showing the gain of consistency

--- figures.py
+++ figures.py
@@ -20,7 +20,7 @@ mpl.rcParams["grid.linewidth"] = '0.5'
 DEFAULT_z_levels = np.linspace(0, 1500, 41)
 
 def plot_FD(axes, sf_scheme, dt=60., N=1680,
-        z_levels=DEFAULT_z_levels, name=None):
+        z_levels=DEFAULT_z_levels, name=None, style={}):
     if name == None:
         name = sf_scheme
     M = z_levels.shape[0] - 1
@@ -32,13 +32,13 @@ def plot_FD(axes, sf_scheme, dt=60., N=1680,
     u, TKE, ustar, shear = simulator.FD(u_t0=u_0,
             sf_scheme=sf_scheme, forcing=forcing)
 
-    axes[0].plot(np.real(u), simulator.z_half[:-1])
-    axes[1].plot(np.imag(u), simulator.z_half[:-1])
-    axes[2].plot(TKE, simulator.z_full, label=name)
-    axes[3].plot(dt*np.array(range(len(ustar))), ustar, "+")
+    axes[0].plot(np.real(u), simulator.z_half[:-1], **style)
+    axes[1].plot(np.imag(u), simulator.z_half[:-1], **style)
+    axes[2].plot(TKE, simulator.z_full, **style, label=name)
+    axes[3].plot(dt*np.array(range(len(ustar))), ustar, **style)
 
 def plot_FV(axes, sf_scheme, delta_sl, dt=60., N=1680,
-        z_levels=DEFAULT_z_levels, name=None):
+        z_levels=DEFAULT_z_levels, name=None, style={}):
     if name == None:
         name = sf_scheme
     M = z_levels.shape[0] - 1
@@ -76,18 +76,18 @@ def plot_FV(axes, sf_scheme, delta_sl, dt=60., N=1680,
     z_fv, u_fv = simulator.reconstruct_FV(u,
             phi, sf_scheme, delta_sl=delta_sl)
 
-    axes[0].plot(np.real(u_fv), z_fv)
-    axes[1].plot(np.imag(u_fv), z_fv)
-    axes[2].plot(TKE, simulator.z_half, label=name)
-    axes[3].plot(dt*np.array(range(len(ustar))), ustar, "+")
+    axes[0].plot(np.real(u_fv), z_fv, **style)
+    axes[1].plot(np.imag(u_fv), z_fv, **style)
+    axes[2].plot(TKE, simulator.z_half, **style, label=name)
+    axes[3].plot(dt*np.array(range(len(ustar))), ustar, **style)
 
-def fig_integration_1dekman():
+def fig_consistency_comparison():
     """
         Integrates for 1 day a 1D ekman equation
         with TKE turbulence scheme.
     """
     z_levels= np.linspace(0, 1500, 41)
-    z_levels_les= np.linspace(0, 1500, 4001)
+    z_levels_les= np.linspace(0, 1500, 401)
     # for FV with FV interpretation of sf scheme,
     # the first grid level is divided by 2 so that
     # delta_{sl} is the same in all the schemes.
@@ -97,13 +97,30 @@ def fig_integration_1dekman():
     fig, axes = plt.subplots(1,4, figsize=(7.5, 3.5))
     fig.subplots_adjust(left=0.08, bottom=0.14, wspace=0.7, right=0.99)
     sf_scheme_FV = "FV2 free"
-    # plot_FD(axes, "FD pure", N=N, dt=dt, z_levels=z_levels)
-    plot_FV(axes, "FV2 free", delta_sl=10.,
-            N=N, dt=dt, z_levels=z_levels, name="FV2 free, $\delta_{sl}=10$m")
-    plot_FV(axes, "FV2 free", delta_sl=30.,
-            N=N, dt=dt, z_levels=z_levels, name="FV2 free, $\delta_{sl}=30$m")
-    plot_FV(axes, "FV2 free", delta_sl=100.,
-            N=N, dt=dt, z_levels=z_levels, name="FV2 free, $\delta_{sl}=100$m")
+    col_FDpure = "#488f31"
+    col_FV1 = "#acb75b"
+    col_FVfree = "#de425b"
+    def style(col, linestyle='solid', **kwargs):
+        return {"color": col, "linestyle": linestyle,
+                "linewidth":0.8, **kwargs}
+
+    plot_FD(axes, "FD pure", N=N, dt=dt, z_levels=z_levels,
+            name="FD, M=40", style=style(col_FDpure))
+    plot_FD(axes, "FD pure", N=N, dt=dt, z_levels=z_levels_les,
+            name="FD, M=400", style=style(col_FDpure, "dashed"))
+    plot_FV(axes, "FV1", delta_sl=z_levels[1]/2,
+            N=N, dt=dt, z_levels=z_levels,
+            name="FV1, M=40", style=style(col_FV1))
+    plot_FV(axes, "FV1", delta_sl=z_levels_les[1]/2,
+            N=N, dt=dt, z_levels=z_levels_les,
+            name="FV1, M=400", style=style(col_FV1, "dashed"))
+    plot_FV(axes, "FV2 free", delta_sl=z_levels[1]/2,
+            N=N, dt=dt, z_levels=z_levels,
+            name="FV2 free, M=40", style=style(col_FVfree))
+    plot_FV(axes, "FV2 free", delta_sl=z_levels[1]/2,
+            N=N, dt=dt, z_levels=z_levels_les,
+            name="FV2 free, M=400", style=style(col_FVfree, "dashed"))
+
     axes[0].set_ylim(top=1500., bottom=0.)
     axes[1].set_ylim(top=1500., bottom=0.)
     axes[0].set_xlabel("wind speed (u, $m.s^{-1}$)")
@@ -113,88 +130,12 @@ def fig_integration_1dekman():
     axes[2].set_xlabel("energy (J)")
     axes[2].set_ylabel("height (m)")
     axes[3].set_ylabel("friction velocity (u*, $m.s^{-1}$)")
+    axes[3].set_ylim(top=0.5, bottom=0.38)
     axes[3].set_xlabel("time (s)")
     axes[0].legend(loc="upper right")
     axes[1].legend(loc="upper right")
     axes[2].legend(loc="upper right")
-    show_or_save("fig_integration_1dekman")
-
-
-def fig_stationary_compare():
-    import simulator
-    NUMBER_OF_LEVELS = 100
-    H = 1200.
-    H_cfl = 50.
-    H_bl = 1000.
-    NUMBER_ITERATION = 3000
-    skip_iterations = 10
-    z = np.linspace(1, H, NUMBER_OF_LEVELS)
-
-    errors, profile, viscosity = simulator.stationary_case_KPP(z, H_cfl=H_cfl, H_bl=H_bl, NUMBER_ITERATION=NUMBER_ITERATION)
-    # print(errors)
-    fig, axes = plt.subplots(1,3, figsize=(7.5, 2.5))
-    fig.subplots_adjust(wspace=0.53, left=0.22, bottom=0.22)
-    axes[0].semilogy(range(0, len(errors), skip_iterations),
-            np.array(errors)[::skip_iterations] / np.sqrt(NUMBER_OF_LEVELS) * np.sqrt(H), "k+")
-    axes[0].set_xlabel("iteration")
-    axes[0].set_ylabel(r"$||u_{n+1} - u_n||_2$")
-    axes[1].axhline(H_cfl, color="k", linestyle="dashed")
-    axes[1].axhline(H_bl, color="grey", linestyle="dashed")
-    axes[1].plot(np.real(profile), z, label="u")
-    axes[1].plot(np.imag(profile), z, label="v")
-    axes[1].legend(loc="center")
-    axes[1].set_xlabel("u(z), v(z)")
-    axes[1].set_ylabel("z")
-    axes[2].axhline(H_cfl, linestyle="dashed", color="black", label=r"$\delta_{cfl}$")
-    axes[2].axhline(H_bl, linestyle="dashed", color="grey", label=r"$H_{bl}$")
-    axes[2].plot(viscosity, np.concatenate(([0],(z[1:] + z[:-1])/2)))
-    axes[2].set_xlabel("K(z)")
-    axes[2].set_ylabel("z")
-    axes[2].legend(loc="center")
-    # axes[3].axhline(H_cfl, linestyle="dashed", color="black")
-    # axes[3].axhline(H_bl, linestyle="dashed", color="grey")
-    # axes[3].plot(l, (z[1:] + z[:-1])/2)
-    # axes[3].set_xlabel(r"$l_m(z)$")
-    # axes[3].set_ylabel("z")
-    show_or_save("fig_stationary_test")
-
-def fig_stationary_test():
-    import simulator
-    NUMBER_OF_LEVELS = 1000
-    H = 1200.
-    H_cfl = 50.
-    H_bl = 1000.
-    NUMBER_ITERATION = 1000000
-    z = np.linspace(0, H, NUMBER_OF_LEVELS)
-
-    errors, profile, viscosity, l = simulator.stationary_case_MOST(z, H_cfl=H_cfl, H_bl=H_bl, NUMBER_ITERATION=NUMBER_ITERATION)
-    # print(errors)
-    fig, axes = plt.subplots(1,4, figsize=(7.5, 2.5))
-    fig.subplots_adjust(wspace=0.53, left=0.22, bottom=0.22)
-    skip_iterations = 1000
-    axes[0].semilogy(range(0, len(errors), skip_iterations),
-            np.array(errors)[::skip_iterations] / np.sqrt(NUMBER_OF_LEVELS) * np.sqrt(H), "k+")
-    axes[0].set_xlabel("iteration")
-    axes[0].set_ylabel(r"$||u_{n+1} - u_n||_2$")
-    axes[1].axhline(H_cfl, color="k", linestyle="dashed")
-    axes[1].axhline(H_bl, color="grey", linestyle="dashed")
-    axes[1].plot(np.real(profile), z, label="u")
-    axes[1].plot(np.imag(profile), z, label="v")
-    axes[1].legend(loc="center")
-    axes[1].set_xlabel("u(z), v(z)")
-    axes[1].set_ylabel("z")
-    axes[2].axhline(H_cfl, linestyle="dashed", color="black", label=r"$\delta_{cfl}$")
-    axes[2].axhline(H_bl, linestyle="dashed", color="grey", label=r"$H_{bl}$")
-    axes[2].plot(viscosity, (z[1:] + z[:-1])/2)
-    axes[2].set_xlabel("K(z)")
-    axes[2].set_ylabel("z")
-    axes[2].legend(loc="center")
-    axes[3].axhline(H_cfl, linestyle="dashed", color="black")
-    axes[3].axhline(H_bl, linestyle="dashed", color="grey")
-    axes[3].plot(l, (z[1:] + z[:-1])/2)
-    axes[3].set_xlabel(r"$l_m(z)$")
-    axes[3].set_ylabel("z")
-    show_or_save("fig_stationary_test")
+    show_or_save("fig_consistency_comparison")
 
 def get_discrete_freq(N, dt, avoid_zero=True):
     """
