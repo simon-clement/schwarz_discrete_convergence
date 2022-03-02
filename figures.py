7bf90f7e2aa085ef2e74bdc58fab98a1e9f7432c
Simon Clement <simon.clement@grenoble-inp.org>
2022-03-02 08:43:54 +0100

    parallelized optiRatesL2

--- figures.py
+++ figures.py
@@ -1248,8 +1248,11 @@ def fig_optiRatesL2():
     all_rates = rho_c_c, rho_BE_c, rho_BE_FV, rho_BE_FD
     all_ocean = OceanBEFV, OceanBEFV, OceanBEFV, OceanBEFD
     all_atmosphere = AtmosphereBEFV, AtmosphereBEFV, AtmosphereBEFV, AtmosphereBEFD
-    
-    optiRatesGeneralL2(all_rates, all_ocean, all_atmosphere, "BE", caracs=caracs)
+    import multiprocessing
+    p = multiprocessing.Process(target=optiRatesGeneralL2,
+            args=(all_rates, all_ocean, all_atmosphere,
+            "BE", caracs))
+    p.start()
 
     ###########################
     # Pade
